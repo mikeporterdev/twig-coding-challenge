6453931582162aaf3bdadd7d5ea22e5e578a1f70
Michael Porter <m_porter@live.co.uk>
2019-07-12 20:12:32 +0100

    Implemented splitArray function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-
+/**
+ * A method to split an array into an array of arrays with the size splitSize.
+ * @param array - The array to split - Is not mutated by this function.
+ * @param splitSize - Defines the max size of each internal array. Should be a positive number greater than 0
+ * @returns A new array of arrays, split by the splitSize
+ */
 export function splitArray<T>(array: T[] | null | undefined, splitSize: number): T[][] {
-  return [];
+  if (!array || splitSize <= 0) {
+    return [];
+  }
+
+  const chunkedArray: T[][] = [];
+
+  // Note that this iterates by splitSize, not by i++
+  for (let i = 0; i < array.length; i += splitSize) {
+    chunkedArray.push(array.slice(i, i + splitSize));
+  }
+
+  return chunkedArray;
 }
