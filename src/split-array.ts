f73518096fd8d34ce96df98bb5cad5f8526d2461
Michael Porter <m_porter@live.co.uk>
2019-07-12 22:31:22 +0100

    Comment grammar

--- src/split-array.ts
+++ src/split-array.ts
@@ -3,8 +3,8 @@ import chunk = require('lodash.chunk');
 /**
  * A method to split an array into an array of arrays with the size splitSize.
  * @param array - The array to split - Is not mutated by this function.
- * @param splitSize - Defines the max size of each internal array. Should be a positive number greater than 0
- * @returns A new array of arrays, split by the splitSize
+ * @param splitSize - Defines the max size of each internal array. Should be a positive number greater than 0.
+ * @returns A new array of arrays, split by the splitSize.
  */
 export function splitArray<T>(array: T[] | null | undefined, splitSize: number): T[][] {
   if (!array || splitSize <= 0) {
