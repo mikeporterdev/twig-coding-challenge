38160844884c5bcc874f0815cbe634e01a675084
Michael Porter <m_porter@live.co.uk>
2019-07-12 20:21:29 +0100

    Grouped invalid input tests in a describe and added a negative splitSize test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,19 +1,26 @@
 import { splitArray, splitArrayLodash } from '../index';
 
 describe('SplitArray function', () => {
-  it('should handle null array', function() {
-    expect(splitArray(null, 1)).toStrictEqual([]);
-    expect(splitArrayLodash(null, 1)).toStrictEqual([]);
-  });
+  describe('when given invalid inputs', function() {
+    it('should handle null array', function() {
+      expect(splitArray(null, 1)).toStrictEqual([]);
+      expect(splitArrayLodash(null, 1)).toStrictEqual([]);
+    });
 
-  it('should handle undefined array', function() {
-    expect(splitArray(undefined, 1)).toStrictEqual([]);
-    expect(splitArrayLodash(undefined, 1)).toStrictEqual([]);
-  });
+    it('should handle undefined array', function() {
+      expect(splitArray(undefined, 1)).toStrictEqual([]);
+      expect(splitArrayLodash(undefined, 1)).toStrictEqual([]);
+    });
 
-  it('should handle empty array', function() {
-    expect(splitArray([], 1)).toStrictEqual([]);
-    expect(splitArrayLodash([], 1)).toStrictEqual([]);
+    it('should handle empty array', function() {
+      expect(splitArray([], 1)).toStrictEqual([]);
+      expect(splitArrayLodash([], 1)).toStrictEqual([]);
+    });
+
+    it('should handle negative splitSize', function() {
+      expect(splitArray([1], -1)).toStrictEqual([]);
+      expect(splitArrayLodash([1], -1)).toStrictEqual([]);
+    });
   });
 
   it('should split array into multiple arrays of input size', function() {
@@ -22,8 +29,8 @@ describe('SplitArray function', () => {
   });
 
   it('should return two arrays when given an array of size 4 and split size of 2', function() {
-    expect(splitArray([1, 2, 3, 4], 2)).toStrictEqual([[1, 2], [3, 4]])
-    expect(splitArrayLodash([1, 2, 3, 4], 2)).toStrictEqual([[1, 2], [3, 4]])
+    expect(splitArray([1, 2, 3, 4], 2)).toStrictEqual([[1, 2], [3, 4]]);
+    expect(splitArrayLodash([1, 2, 3, 4], 2)).toStrictEqual([[1, 2], [3, 4]]);
   });
 
   it('should return two arrays with third element in remainder when given an array of size 3 and split size of 2', function() {
